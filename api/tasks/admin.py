8de1b0794a48213e57ee2f8e6d0dc222e8237e71
RuaPedro <pedroruaaa@gmail.com>
2026-02-07 09:05:37 -0800

    update de admin tasks app para orm

--- api/tasks/admin.py
+++ api/tasks/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import Project, Tag, Task, TaskTag
 
-# Register your models here.
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("name", "created_at", "updated_at")
+    search_fields = ("name", "description")
+    ordering = ("-created_at",)
+    
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "created_at", "updated_at")
+    search_fields = ("name",)
+    ordering = ("cerated_at",)
+    
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "status",
+        "priority",
+        "due_date",
+        "created_by",
+        "asigned_to"
+    )
+    list_filter = ("status", "priority", "assigned _to", "is_locked")
+    search_fields = ("title", "asigned_to", "tags")
+    autocomplete_fields = ("project", "created_by", "assigned_to")
+    ordering = ("-created_at")
+    date_hierarchy = "created_at"
+    
+@admin.register(TaskTag)
+class TaskTagAdmin(admin.ModelAdmin):
+    list_display = ("task", "tag", "created_at", "updated_at")
+    search_fields = ("task__title", "tag__name")
+    autocomplete_fields = ("task", "tag")
+    ordering = ("-created_at")
+    
